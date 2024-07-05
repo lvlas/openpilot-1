a233457d41f475049804a87c6623880af9b5720c
John <4vanetten@gmail.com>
2024-07-04 20:02:15 -0600

    Revert "lets try V2"
    This reverts commit d2cb0780dce2d117b57abacdff93609201c44bde.

--- selfdrive/car/chrysler/long_carcontroller_v2.py
+++ selfdrive/car/chrysler/long_carcontroller_v2.py
@@ -1,10 +1,8 @@
 import math
 
 from cereal import car
-from common.conversions import Conversions as CV
 from openpilot.common.numpy_fast import clip
-from openpilot.selfdrive.car.chrysler.long_carcontroller import LongCarController
-from openpilot.selfdrive.car.chrysler.interface import CarInterface
+from selfdrive.car.chrysler.long_carcontroller import LongCarController
 
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
@@ -20,16 +18,6 @@ ROLLING_RESISTANCE_COEFF = 46.31 / 5500 # Target Coef A (lbf) / Equivalent Test 
 VEHICLE_MASS = 2495 # kg
 GRAVITY = 9.81 # m/s^2
 AIR_DENSITY = 1.225 # kg/m3 (sea level air density of dry air @ 15° C)
-
-# adjust for hills and towing
-TORQ_ADJUST_THRESHOLD = 0.3
-START_ADJUST_ACCEL_FRAMES = 100
-ADJUST_ACCEL_COOLDOWN_MAX = 1
-CAN_DOWNSHIFT_ACCEL_FRAMES = 200
-TORQ_BRAKE_MAX = -0.1
-
-COAST_WINDOW = CV.MPH_TO_MS * 2
-LOW_WINDOW = CV.MPH_TO_MS * 5
 
 def calc_motion_force(aEgo, road_pitch):
   force_parallel = VEHICLE_MASS * aEgo
@@ -54,29 +42,36 @@ def calc_engine_torque(accel, pitch, transmission_gear, drag_force):
   engine_torque = wheel_torque / FINAL_DRIVE_RATIOS[int(transmission_gear)-1]
   return engine_torque
 
-def create_acc_commands(packer, counter_offset, das_3, enabled, long_active, gas, max_gear, brakes, brake_prep, starting, stopping):
+def create_acc_commands(packer, counter_offset, das_3, enabled, long_active, gas, brakes, brake_prep, starting, stopping):
   commands = []
 
-  das_3_values = das_3.copy()
-  das_3_values['ACC_AVAILABLE'] = 1
-  das_3_values['ACC_ACTIVE'] = enabled
-  das_3_values['COUNTER'] = (das_3['COUNTER'] + counter_offset) % 0x10
-  das_3_values['ACC_STANDSTILL'] = stopping and long_active
-  das_3_values['ACC_GO'] = starting and long_active
-
-  if brakes is not None and long_active:
-    das_3_values['ACC_DECEL_REQ'] = long_active
-    das_3_values['ACC_DECEL'] = brakes
-    das_3_values['ACC_BRK_PREP'] = brake_prep
-
-  if gas is not None and long_active:
-    das_3_values['ENGINE_TORQUE_REQUEST_MAX'] = long_active
-    das_3_values['ENGINE_TORQUE_REQUEST'] = gas
-
-  if max_gear is not None:
-    das_3_values['GR_MAX_REQ'] = max_gear
+  das_3_values = {
+    'COUNTER': (das_3['COUNTER'] + counter_offset) % 0x10,
+    'ACC_AVAILABLE': 1,
+    'ACC_ACTIVE': enabled,
+    'ACC_DECEL_REQ': brakes < 0.0 if long_active else 0,
+    'ACC_DECEL': brakes,
+    'ENGINE_TORQUE_REQUEST_MAX': brakes >= 0.0 if long_active else 0,
+    'ENGINE_TORQUE_REQUEST': gas,
+    'ACC_STANDSTILL': stopping if long_active else 0,
+    'ACC_GO': starting if long_active else 0,
+    # TODO: does this have any impact on fuel economy when engine braking?
+    # TODO: does this disable auto engine start/stop?
+    'DISABLE_FUEL_SHUTOFF': 1,
+    # TODO: does this have any impact on ACC braking responsiveness?
+    # TODO: does this cause a fault if set for too long?
+    'ACC_BRK_PREP': brake_prep,
+    # TODO: does this have any impact on ACC braking responsiveness?
+    #'COLLISION_BRK_PREP': ?,
+  }
 
   commands.append(packer.make_can_msg("DAS_3", 0, das_3_values))
+
+  # das_5_values = {
+  #   "FCW_STATE": 0, # off
+  #   "FCW_DISTANCE": 0, # off
+  # }
+  # commands.append(packer.make_can_msg("DAS_5", 0, das_5_values))
 
   return commands
 
@@ -85,9 +80,6 @@ class LongCarControllerV2(LongCarController):
     super().__init__(CP, params, packer)
 
     self.last_gas = 0
-    self.under_accel_frame_count = 0
-    self.torq_adjust = 0.
-    self.max_gear = None
 
   def acc(self, longitudinalPlan, frame, CC, CS, can_sends):
     counter_changed = CS.das_3['COUNTER'] != self.last_das_3_counter
@@ -95,19 +87,16 @@ class LongCarControllerV2(LongCarController):
 
     if not CC.enabled or not CS.longControl:
       self.last_gas = 0
-      self.under_accel_frame_count = 0
-      self.torq_adjust = 0.
-      self.max_gear = None
       return None
 
     # longitudinal
     if self.CP.openpilotLongitudinalControl:
-      accel = clip(CC.actuators.accel, self.params.ACCEL_MIN, CarInterface.accel_max(CS))
+      accel = clip(CC.actuators.accel, self.params.ACCEL_MIN, self.params.ACCEL_MAX)
 
       starting = False
       stopping = False
-      gas = None
-      brakes = None
+      gas = CS.torqMin
+      brakes = self.params.INACTIVE_ACCEL
       if not CC.longActive:
         self.last_gas = max(CS.engine_torque, 0.0)
 
@@ -117,49 +106,21 @@ class LongCarControllerV2(LongCarController):
 
         pitch = CC.orientationNED[1] if len(CC.orientationNED) > 1 else 0
         drag_force = calc_drag_force(CS.engine_torque, CS.transmission_gear, pitch, CS.out.aEgo, CS.out.vEgo)
-        torque = calc_engine_torque(accel, pitch, CS.transmission_gear, drag_force)
-        torque = torque + (self.torq_adjust if torque > 0 else 0)  # adjust for hills and towing
-        gas = clip(min(torque, self.last_gas + 2), CS.torqMin, CS.torqMax)
+        gas = clip(calc_engine_torque(accel, pitch, CS.transmission_gear, drag_force), CS.torqMin, CS.torqMax)
+        gas = min(gas, self.last_gas + 2)
         self.last_gas = max(gas, 0)
-
-        vTarget = longitudinalPlan.speeds[-1]
-        speed_to_far_off = abs(CS.out.vEgo - vTarget) > COAST_WINDOW
-        engine_brake = TORQ_BRAKE_MAX < accel < 0.0 and not speed_to_far_off and vTarget > LOW_WINDOW and torque > CS.torqMin
-
-        if accel >= 0.0 or engine_brake:
-          # adjust for hills and towing
-          accel_offset = accel - CS.out.aEgo
-
-          if accel_offset > TORQ_ADJUST_THRESHOLD:
-            self.under_accel_frame_count = self.under_accel_frame_count + 1  # inc under accelerating frame count
-            if frame - self.under_accel_frame_count > START_ADJUST_ACCEL_FRAMES:
-              self.torq_adjust += accel_offset * (CarInterface.ACCEL_MAX / CarInterface.accel_max(CS))
-
-          if self.under_accel_frame_count == 0:
-            self.max_gear = None
-          elif self.under_accel_frame_count > CAN_DOWNSHIFT_ACCEL_FRAMES:
-            if CS.out.vEgo < accel - COAST_WINDOW / CarInterface.accel_max(CS) \
-              and CS.out.aEgo < CarInterface.accel_max(CS) / 5 \
-              and gas > CS.torqMax * 0.98:  # Time to downshift?
-              if CS.transmission_gear > 3 and CS.gasRpm < 4500:
-                self.max_gear = CS.transmission_gear - 1
-                self.under_accel_frame_count = 0
-        else:
-          self.max_gear = None
+        # TODO: handle road pitch sometimes causing negative accel to be positive gas (uphill)
+        if accel < 0.0:
+          gas = 0.0
           brakes = min(accel, 0)
 
-        if accel < 0.0:
-          self.under_accel_frame_count = 0
-          self.torq_adjust = max(0, self.torq_adjust - max(accel * 10, ADJUST_ACCEL_COOLDOWN_MAX))
-
-      brake_prep = brakes is not None and len(longitudinalPlan.accels) and longitudinalPlan.accels[0] - longitudinalPlan.accels[-1] > 1.0
+      brake_prep = brakes < 0.0 and len(longitudinalPlan.accels) and longitudinalPlan.accels[0] - longitudinalPlan.accels[-1] > 1.0
       can_sends.extend(create_acc_commands(self.packer,
                                            2 if counter_changed else 3,
                                            CS.das_3,
                                            CC.enabled,
                                            CC.longActive,
                                            gas,
-                                           self.max_gear,
                                            brakes,
                                            brake_prep,
                                            starting,
