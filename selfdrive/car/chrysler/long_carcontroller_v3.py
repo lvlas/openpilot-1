f009d76d8d40bdaba602b50f9ccf7705e12e46d9
John <4vanetten@gmail.com>
2024-07-05 20:14:25 -0600

    derp

--- selfdrive/car/chrysler/long_carcontroller_v3.py
+++ selfdrive/car/chrysler/long_carcontroller_v3.py
@@ -81,7 +81,7 @@ class LongCarControllerV3(LongCarController):
     self.vehicleMass = CP.mass
     self.max_gear = None
 
-    self.finalDriveRatios = [x * CP.AXEL_RATIO for x in CP.GEAR_RATIOS]
+    self.finalDriveRatios = [x * CP.axleRatio for x in CP.gearRatios]
 
 
   # T = (mass x accel x velocity x 1000)/(.105 x Engine rpm)
