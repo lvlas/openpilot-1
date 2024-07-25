4ec2f460216e3ba59c00696a0cd4341c923c8d04
John <4vanetten@gmail.com>
2024-07-24 20:09:16 -0600

    back to xps code

--- selfdrive/car/chrysler/radar_interface.py
+++ selfdrive/car/chrysler/radar_interface.py
@@ -75,25 +75,17 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[trackId].trackId = trackId
         self.pts[trackId].aRel = float('nan')
         self.pts[trackId].yvRel = float('nan')
-        self.pts[trackId].measured = True
 
-      if 'LONG_DIST' in cpt:
+      if 'LONG_DIST' in cpt:  # c_* message
         self.pts[trackId].dRel = cpt['LONG_DIST']  # from front of car
-      if 'REL_SPEED' in cpt:
+        self.pts[trackId].yRel = cpt['LAT_ANGLE']  # in car frame's y axis, left is positive
+        self.pts[trackId].yRel = tan(self.pts[trackId].yRel) * self.pts[trackId].dRel * self.yRel_multiplier
+      else:  # d_* message
         self.pts[trackId].vRel = cpt['REL_SPEED']
-      if 'MEASURED' in cpt:
         self.pts[trackId].measured = bool(cpt['MEASURED'])
-      if 'LAT_DIST' in cpt:
-        self.pts[trackId].yRel = cpt['LAT_DIST'] * self.yRel_multiplier
-      elif 'LAT_ANGLE' in cpt and 'LONG_DIST' in cpt:
-        self.pts[trackId].yRel = tan(cpt['LAT_ANGLE']) * cpt['LONG_DIST'] * self.yRel_multiplier
-
-      # TODO: Figure out how to provide this to log
-      # if 'PROBABILITY' in cpt:
-      #   self.pts[trackId].modelProb = float(cpt['PROBABILITY']) / 255
 
     # We want a list, not a dictionary. Filter out LONG_DIST==0 because that means it's not valid.
-    ret.points = [x for x in self.pts.values() if x.dRel != 0]
+    ret.points = [x for x in self.pts.values() if x.measured and 290 > x.dRel > 260]
 
     self.updated_messages.clear()
     return ret
