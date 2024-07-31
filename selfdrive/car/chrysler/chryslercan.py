6adc05c60f50915637a1e3667fe8f0166aa7cfe1
John <4vanetten@gmail.com>
2024-07-30 19:07:30 -0600

    use latActive to control WP

--- selfdrive/car/chrysler/chryslercan.py
+++ selfdrive/car/chrysler/chryslercan.py
@@ -54,12 +54,12 @@ def create_lkas_hud(packer, CP, lat_active, hud_alert, hud_count, car_model, aut
   return packer.make_can_msg("DAS_6", 0, values)
 
 
-def create_lkas_command(packer, CP, apply_steer, lkas_control_bit):
+def create_lkas_command(packer, CP, apply_steer, lkas_control_bit, wp_control, wp_active):
   # LKAS_COMMAND Lane-keeping signal to turn the wheel
   enabled_val = 2 if CP.carFingerprint in RAM_CARS else 1
   values = {
-    "WP_CONTROL": 1,
-    "WP_ACTIVE": 1,
+    "WP_CONTROL": 1 if wp_control else 0,
+    "WP_ACTIVE": 1 if wp_active else 0,
     "STEERING_TORQUE": apply_steer,
     "LKAS_CONTROL_BIT": enabled_val if lkas_control_bit else 0,
   }
