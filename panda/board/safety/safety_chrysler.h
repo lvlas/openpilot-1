9fe6a50fd242e88ae0a068da6354a17774498d84
John <4vanetten@gmail.com>
2024-08-18 15:18:43 -0600

    fix controls

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -193,9 +193,10 @@ static void chrysler_rx_hook(const CANPacket_t *to_push) {
   // enter controls on rising edge of ACC, exit controls on ACC off
   const int das_3_bus = (chrysler_platform == CHRYSLER_PACIFICA) ? 0 : 2;
   if ((bus == das_3_bus) && (addr == chrysler_addrs->DAS_3)) {
-    bool cruise_engaged = GET_BIT(to_push, 21U) || ((alternative_experience & ALT_EXP_AOLC_ENABLED) && GET_BIT(to_push, 20U));
-    cruise_engaged = true; // FIXME: Check if experimental is enabled && !GET_BIT(to_push, 21U)
-    pcm_cruise_check(cruise_engaged);
+    // FIXME: Check if long is enabled && !GET_BIT(to_push, 21U)
+    //bool cruise_engaged = GET_BIT(to_push, 21U) || ((alternative_experience & ALT_EXP_AOLC_ENABLED) && GET_BIT(to_push, 20U));
+    //pcm_cruise_check(cruise_engaged);
+    pcm_cruise_check(controls_allowed); pcm_cruise_check(true); // for now, we are always in control
   }
 
   // TODO: use the same message for both
