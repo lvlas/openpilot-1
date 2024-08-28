793fc9c6b8bbba07c829867f15921732a8358754
lvlas <106373597+lvlas@users.noreply.github.com>
2024-08-29 01:38:32 +0200

    Steererror Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -118,7 +118,7 @@ class CarInterface(CarInterfaceBase):
 
     #ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
 
-    ret.steerError = self.CC.steerErrorMod
+    #ret.steerError = self.CC.steerErrorMod
     
     # events
     events = self.create_common_events(ret, extra_gears=[car.CarState.GearShifter.low])
