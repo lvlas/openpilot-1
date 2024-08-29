7a841b742dc015339261f9f86cf45909608232e5
lvlas <106373597+lvlas@users.noreply.github.com>
2024-08-29 02:25:40 +0200

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -160,7 +160,7 @@ class CarController(CarControllerBase):
     self.apaActive = CS.apasteerOn and self.steer_type == 2
     #novy    
       
-    can_sends.append(chryslercan.create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit, self.steerNoMinimum, CC.latActive, self.gone_fast_yet))
+    can_sends.append(chryslercan.create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit, self.steerNoMinimum, CC.latActive, False))
 
     if CC.enabled:
       # auto set profile
